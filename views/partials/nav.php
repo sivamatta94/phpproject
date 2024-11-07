c87e152df653b2f9681820c8342369eb365e5128
sivamatta94 <sivamech301@gmail.com>
2024-11-07 16:24:40 +0100

    com

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -9,12 +9,12 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/test/index.php"
+                        <a href="/PHPPROJECT/index.php"
                             class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                             aria-current="page">Home</a>
-                        <a href="/test/about.php"
+                        <a href="/PHPPROJECT/about.php"
                             class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                        <a href="/test/contact.php"
+                        <a href="/PHPPROJECT/contact.php"
                             class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
 
                     </div>
